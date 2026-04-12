e1c202250c994fc130bd2faf0f9611fb240bf82c
AISUKI_MAIN <corentin25besnier@gmail.com>
2026-04-12 17:00:36 +0200

    Fixed shader referencement for Build Fixed PlayerHealth (after buying reinforcement upgrade, the first collectible collected throw back the health to playerConfig.MaxHealth)

--- Assets/Components/InGame/Chunks/Interactables/Collectibles/OutlineController.cs
+++ Assets/Components/InGame/Chunks/Interactables/Collectibles/OutlineController.cs
@@ -1,35 +1,41 @@
 using UnityEngine;
 
-/// <summary>
-/// Creates an outline on attached meshRenderers
-/// </summary>
-[RequireComponent(typeof(MeshFilter))]
-public class OutlineController : MonoBehaviour
+namespace Assets.Components.InGame.Chunks.Interactables.Collectibles
 {
-	[SerializeField] private Color _outlineColor = Color.white;
-	[SerializeField] private float _outlineWidth = 0.05f;
-
-	private GameObject _outlineMesh;
-
-	private void Start()
+	[RequireComponent(typeof(MeshFilter))]
+	public class OutlineController : MonoBehaviour
 	{
-		// Crée un duplicata du mesh juste pour l'outline
-		_outlineMesh = new GameObject("Outline");
-		_outlineMesh.transform.SetParent(transform, false);
+		[SerializeField] private Color _outlineColor = Color.white;
+		[SerializeField] private float _outlineWidth = 0.05f;
+		[SerializeField] private Shader _outlineShader;
 
-		MeshFilter mf = _outlineMesh.AddComponent<MeshFilter>();
-		mf.mesh = GetComponent<MeshFilter>().mesh;
+		private GameObject _outlineMesh;
 
-		MeshRenderer mr = _outlineMesh.AddComponent<MeshRenderer>();
-		Material outlineMat = new Material(Shader.Find("Custom/OutlineOnly"));
-		outlineMat.SetColor("_OutlineColor", _outlineColor);
-		outlineMat.SetFloat("_OutlineWidth", _outlineWidth);
-		mr.material = outlineMat;
-	}
+		private void Start()
+		{
+			if (_outlineShader == null)
+			{
+				Debug.LogError("[OutlineController] Outline shader non assigné !");
+				return;
+			}
 
-	private void OnDestroy()
-	{
-		if (_outlineMesh != null)
-			Destroy(_outlineMesh);
+			_outlineMesh = new GameObject("Outline");
+			_outlineMesh.transform.SetParent(transform, false);
+
+			MeshFilter mf = _outlineMesh.AddComponent<MeshFilter>();
+			mf.mesh = GetComponent<MeshFilter>().mesh;
+
+			MeshRenderer mr = _outlineMesh.AddComponent<MeshRenderer>();
+			Material outlineMat = new Material(_outlineShader);
+			outlineMat.SetColor("_OutlineColor", _outlineColor);
+			outlineMat.SetFloat("_OutlineWidth", _outlineWidth);
+			mr.material = outlineMat;
+		}
+
+		private void OnDestroy()
+		{
+			if (_outlineMesh != null)
+				Destroy(_outlineMesh);
+		}
 	}
 }
